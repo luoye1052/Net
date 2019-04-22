aedfd940f4c1f3e4728e8af1c01cfe6f317e71c9
于兵 <bing.yu@emaxdata.com>
2019-04-22 11:19:09 +0800

    set pod

---
+++ net/net/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  net
+//
+//  Created by yubing on 2019/4/22.
+//  Copyright © 2019 Emadata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
